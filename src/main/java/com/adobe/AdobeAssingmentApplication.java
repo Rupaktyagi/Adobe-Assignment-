4cb567e14834d074d36599339e9b6881f2070fba
Rupak Tyagi <rupaktyagi21@gmail.com>
2023-04-10 09:37:25 +0530

    assignment added

---
+++ src/main/java/com/adobe/AdobeAssingmentApplication.java
@@ -0,0 +1,13 @@
+package com.adobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdobeAssingmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdobeAssingmentApplication.class, args);
+	}
+
+}
